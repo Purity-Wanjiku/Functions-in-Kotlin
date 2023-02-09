e93657f648978d2a302860d121e7df7ae3e0c1ce
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-02-09 16:46:14 +0300

    Created functions in Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+import java.awt.geom.Area
+import kotlin.jvm.internal.AdaptedFunctionReference
+
+fun main() {
+ printname("Ada")
+    printname("fuy")
+    printname("Ada")
+    printname("huy")
+    modulus(45,65)
+modulus(65,3)
+    println()
+    fact("I am a long distance bike rider")
+
+}
+fun printname (name: String){
+println("Hello"+" "+ name)
+}
+fun modulus(num1: Int, num2: Int){
+    var div = num1 % num2
+    println(div)
+}
+fun sum (num1: Int, num2: Int, num3: Int, num4: Int){
+    var add = num1 + num2 + num3 + num4
+    println(add)
+}
+fun fact(myself: String){
+    println(myself)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
